f793024ebe3d63a44f0e63f48deac009c359b19f
ugurpala <78415316+ugurpala@users.noreply.github.com>
2021-02-06 11:58:50 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,15 @@
-#Explain your work
+"""
+Homework 1
+Create a list and swap the second half of the list with the first
+half of the list and print this list on the screen.
 
-#Question 1
-for x in range(a):
-	print(a)
+"""
+
+nums = list(range(1,5))
+print("Original list:")
+print(nums)
+print("\n")
+
+nums[0], nums[1], nums[2], nums[3] = nums[2], nums[3], nums[0], nums[1]
+print("New list:")
+print(nums)
