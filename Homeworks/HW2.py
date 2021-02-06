1a98e63eb01ea649c31d1e651fd5767fb92ca1a3
ugurpala <78415316+ugurpala@users.noreply.github.com>
2021-02-06 12:03:49 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,23 @@
-#Explain your work
+"""
+Homework 2
 
-#Question 1
-for x in range(a):
-	print(a)
+Ask the user to input a single digit integer to a variable a variable 'n'
+Then, print out all of the even numbers from 0 to n (including n)
+
+"""
+
+n = int(input("Please enter a single digit integer: "))
+
+while n<0 or n>9:
+
+    print("Invalid number!!")
+    n = int(input("Please enter a single digit integer: "))
+    
+even_numbers = list()
+
+for i in range(0,n+1):
+    if i % 2 == 0:
+        even_numbers.append(i)
+        print(i)
+print("All of the even numbers from 0 to {} = {}".format(n, even_numbers))
+
